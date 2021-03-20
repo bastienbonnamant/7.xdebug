8f12add5f0e22357df6b007f80d2952d13484ec4
Bastien Bonnamant <79262493+bastienbonnamant@users.noreply.github.com>
2021-03-20 13:03:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ]
+    "Bastien" => [
+        "city" => "Strasbourg",
+        "weapon" => "axe",
+    ]
+];
+var_dump($characters);
+
+
+
+?>
